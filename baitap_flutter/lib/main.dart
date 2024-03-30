870fa78d061616e032840006da502889d0ff02f2
Đoàn Thành Lợi <100418351+ThanhLoi-300@users.noreply.github.com>
2024-03-30 20:55:51 +0700

    init

---
+++ baitap_flutter/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  "Firebase",
+                )),
+
+            const SizedBox( width: 10, ),
+
+            ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  "Camera",
+                )),
+            
+            const SizedBox( width: 10, ),
+
+            ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  "Gallery",
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
